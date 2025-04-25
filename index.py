d2630aa0790b3ad8485d2f71bab7620d177d068c
main <claudia.raphael73@gmail.com>
2025-04-25 16:55:00 -0300

    import requirements.txt

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-import os
+import requests
+
+API_KEY = "your_api_key_here"
+API_URL = "https://api.example.com/data"
+
+city = input("Enter the name of the city: ")
+request_url = f"{API_URL}?q={city}&appid={API_KEY}"
