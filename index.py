e725989eed881f6bb9623d0ac913be583d824d1c
main <claudia.raphael73@gmail.com>
2025-04-25 17:48:01 -0300

    Check if API call was successful

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
 import requests
 
 API_KEY = "668b1bdb2090da8f8d244325f4e729a6" # test key
-API_URL = "https://dashboard.openweather.co.uk/home"
+API_URL = "http://api.openweathermap.org/data/2.5/forecast"
+
+# Example API call:
+# http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 
 city = input("Enter the name of the city: ")
-request_url = f"{API_URL}?q={city}&appid={API_KEY}"
+
+API_CALL = f"{API_URL}?q={city}&appid={API_KEY}"
+
+### Make the API call
+response = requests.get(API_CALL)
+
+# Check if the API call was successful (status code 200)
+if response.status_code == 200:
+    data = response.json()
+    print("API call successful!")
